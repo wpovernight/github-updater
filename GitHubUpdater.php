a52650067edfa9907917489983e11e6f91537026
Alexandre Faustino <alexmigf@gmail.com>
2025-02-18 14:34:02 +0000

    v1.1.4

--- GitHubUpdater.php
+++ GitHubUpdater.php
@@ -8,7 +8,7 @@ namespace WPO\GitHubUpdater;
  *
  * @author Ryan Sechrest (modified by WP Overnight)
  * @package WPO\GitHubUpdater
- * @version 1.1.3
+ * @version 1.1.4
  */
 class GitHubUpdater
 {
